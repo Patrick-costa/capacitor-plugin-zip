6500f37d3075cc5b091873e967435bd3a3519e69
Patrick Monteiro <patrickmonteiro@Brunos-Mac-mini.local>
2024-03-18 18:14:03 -0300

    finally implementation ios

--- ios/Plugin/ZipPlugin.m
+++ ios/Plugin/ZipPlugin.m
@@ -3,7 +3,7 @@
 
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
-CAP_PLUGIN(ZipPlugin, "ZipPlugin",
+CAP_PLUGIN(ZipPlugin, "Zip",
            CAP_PLUGIN_METHOD(zip, CAPPluginReturnCallback);
            CAP_PLUGIN_METHOD(unZip, CAPPluginReturnCallback);
 )
